d37846613ee335cfd6c9cc2931f1f9dcb7892f66
jack <likerockman102@hotmail.com>
2025-04-06 20:44:29 +0800

    Java客户端-direct交換機

---
+++ consumer/src/main/java/com/itheima/consumer/MqListener/SpringRabbitListener.java
@@ -0,0 +1,66 @@
+package com.itheima.consumer.MqListener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+
+@Component
+@Slf4j
+public class SpringRabbitListener {
+    // 利用RabbitListener來聲明要監聽的隊列信息
+    // 將來一旦監聽的隊列中有了消息，就會推送給當前服務，調用當前方法，處理消息。
+    // 可以看到方法體中接收的就是消息體的內容
+    @RabbitListener(queues = "simple.queue")
+    public void listenSimpleQueue(String msg){
+        System.out.println("消費者收到 simple.queue 的消息：[" + msg + "] ");
+    }
+
+    @RabbitListener(queues = "work.queue")
+    public void listenWorkQueue1(String msg) throws InterruptedException {
+        System.out.println("消費者1 收到了 work.queue 的消息：[" + msg + "] ");
+        Thread.sleep(20);
+    }
+
+    @RabbitListener(queues = "work.queue")
+    public void listenWorkQueue2(String msg) throws InterruptedException {
+        System.err.println("消費者2 收到了 work.queue 的消息.......：[" + msg + "] ");
+        Thread.sleep(200);
+    }
+
+
+
+    @RabbitListener(queues = "fanout.queue1")
+    public void listenFanoutQueue1(String msg) {
+        System.out.println("消費者1接收到Fanout消息：【" + msg + "】");
+    }
+
+    @RabbitListener(queues = "fanout.queue2")
+    public void listenFanoutQueue2(String msg) {
+        System.out.println("消費者2接收到Fanout消息：【" + msg + "】");
+    }
+
+    @RabbitListener(queues = "direct.queue1")
+    public void listenDirectQueue1(String msg) {
+        System.out.println("消費者1接收到direct.queue1的消息：【" + msg + "】");
+    }
+
+    @RabbitListener(queues = "direct.queue2")
+    public void listenDirectQueue2(String msg) {
+        System.out.println("消費者2接收到direct.queue2的消息：【" + msg + "】");
+    }
+//
+//
+//    @RabbitListener(queues = "topic.queue1")
+//    public void listenTopicQueue1(String msg){
+//        System.out.println("消費者1接收到topic.queue1的消息：【" + msg + "】");
+//    }
+//
+//    @RabbitListener(queues = "topic.queue2")
+//    public void listenTopicQueue2(String msg){
+//        System.out.println("消費者2接收到topic.queue2的消息：【" + msg + "】");
+//    }
+
+
+}
--- publisher/src/test/java/com/itheima/publisher/SpringAmqpTest.java
+++ publisher/src/test/java/com/itheima/publisher/SpringAmqpTest.java
@@ -31,7 +31,7 @@ public class SpringAmqpTest {
 
 
     @Test
-    public void testWorkQueue() {
+    public void testWorkQueue() throws InterruptedException {
 
         //1.隊列名
         String queueName = "work.queue";
@@ -41,37 +41,37 @@ public class SpringAmqpTest {
 
             rabbitTemplate.convertAndSend(queueName, message);
 
-
+            Thread.sleep(20);
         }
 
 
     }
     @Test
     public void testFanoutExchange() {
-        // 交换机名称
+        // 交換機名稱
         String exchangeName = "hmall.fanout";
         // 消息
         String message = "hello, everyone!";
         rabbitTemplate.convertAndSend(exchangeName, "", message);
     }
-
+    
     @Test
-    public void testDirectQueue() {
-        // 交换机名称
+    public void testSendDirectQueue() {
+        // 交換機名稱
         String exchangeName = "hmall.direct";
         // 消息
-        String message = "红色警报！日本乱排核废水，导致海洋生物变异，惊现哥斯拉！";
-        // 发送消息
+        String message = "紅色警報！日本亂排核廢水，導致海洋生物變異，驚現哥斯拉！";
+        // 發送消息
         rabbitTemplate.convertAndSend(exchangeName, "blue", message);
     }
 
     @Test
     public void testSendTopicExchange() {
-        // 交换机名称
+        // 交換機名稱
         String exchangeName = "hmall.topic";
         // 消息
         String message = "今天天氣好!";
-        // 发送消息
+        // 發送消息
         rabbitTemplate.convertAndSend(exchangeName, "china.weather", message);
     }
 
